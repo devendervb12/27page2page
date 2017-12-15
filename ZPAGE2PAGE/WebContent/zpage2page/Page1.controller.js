6411874b81957790f463768fc84de64606290fec
devendervb12 <info@s-max.in>
2017-12-15 07:38:27 +0530

    new changes

--- ZPAGE2PAGE/WebContent/zpage2page/Page1.controller.js
+++ ZPAGE2PAGE/WebContent/zpage2page/Page1.controller.js
@@ -2,13 +2,7 @@ sap.ui.controller("zpage2page.Page1", {
 
 
 	onPress : function(){
-    
-		//	var page1View = this.getView();
-		
-	//	var oApp = page1View.getParent();
-		
-	//	oApp.to("idPage12");
-		
+ 	
 		this.getView().getParent().to("idPage12");
 		
 	}
